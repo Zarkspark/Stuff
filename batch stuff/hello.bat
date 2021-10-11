8624e2e0fa33eb8f27e3c2a4309c7b096104d872
Logic <zarkdark87@gmail.com>
2021-10-11 21:56:48 +0100

    batch stuff

--- batch stuff/hello.bat
+++ batch stuff/hello.bat
@@ -1,1 +1,8 @@
-echo Hello world
+@ECHO off
+:: @Echo off is used to hide the actual commands being run in the command line.
+
+SET my_variable=Hello world
+:: This is how you declare a variable in batch.
+
+echo %my_variable%
+:: This just prints the variable. Notice the percentage signs, these are used to signify the variable and not the string: "my_variable".
---
+++ batch stuff/thing.bat
@@ -0,0 +1,5 @@
+@ECHO off
+Rem @ECHO off will hide the things the program does in the command line.
+:x
+START %SystemRoot%\system32\notepad.exe
+goto x
